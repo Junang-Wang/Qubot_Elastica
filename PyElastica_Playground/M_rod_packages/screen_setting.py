30eea48e140d25cf3a9f23ba8d20da304f7429e5
wangjunang <wangjunang94@gmail.com>
2023-12-14 15:38:46 +0800

    fix bug in screen_setting.py

--- PyElastica_Playground/M_rod_packages/screen_setting.py
+++ PyElastica_Playground/M_rod_packages/screen_setting.py
@@ -199,7 +199,7 @@ class screen_setting:
             rescale_pos = 20*rotate_matrix@ rod.position_collection[1:] + (self.constraint_centra).reshape(-1,1) 
             rod_pos = [(rescale_pos[0,i], rescale_pos[1,i]) for i in range(rescale_pos.shape[-1])]
             pygame.draw.lines(self.screen, color=color, closed=False, points = rod_pos, width= width)
-        if current_vec != None:
+        if not current_vec is None:
             print(type(current_vec))
             self.ElectromagnetDraw(current_vec)
         self.clock.tick(fps)
