13e1542ea40b644e2206cd6a40b505d4f947d79b
wangjunang <wangjunang94@gmail.com>
2024-02-23 16:29:45 +0800

    create Neural_network, Training_Loop

--- Modeling eMNS/ReadData.py
+++ Modeling eMNS/ReadData.py
@@ -1,7 +1,27 @@
 import torch 
-import numpy as np
+import glob
 import pandas as pd
 dataFile = 'Modeling eMNS/Data/MagneticField0.txt'
 def ReadData(filename):
     data = torch.tensor(pd.read_table(filename, sep='\s+', skiprows=2).values)
     return data 
+
+def ReadFolder(foldername, filepattern):
+    fileList = glob.glob(foldername+filepattern)
+    fileCounter = len(fileList)
+    
+    for i in range(fileCounter):
+        if i == 0:
+            data_temp = ReadData(filename=fileList[i])
+            [row, col] = data_temp.shape
+            data = torch.empty(row,col,fileCounter)
+            data[:,:,i] = data_temp
+        else:
+            data[:,:,i] = ReadData(filename=fileList[i])
+    
+    return data
+
+        
+            
+
+
