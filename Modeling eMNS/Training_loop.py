38313b4bab5b2b37262e0859920635a4c906be46
windsong57 <1403250637@qq.com>
2024-03-08 15:56:14 +0800

    push files

--- Modeling eMNS/Training_loop.py
+++ Modeling eMNS/Training_loop.py
@@ -297,8 +297,8 @@ def train_part_GM(model,optimizer,train_loader,valid_loader, epochs = 1, learnin
         # print loss during training 
         if verbose and (tt % print_every == 1 or (epoch == epochs -1 and t == len(train_loader) -1) ) :
           print(f'Epoch {epoch:d}, Iteration {tt:d}, loss = {loss.item():.4f}')
-          rmse_val,loss_val,Rsquare = check_rmse_CNN(valid_loader,model,grid_space, device,maxB=maxB,minB=minB)
-          rmse,loss_train,R_TEMP = check_rmse_CNN(train_loader,model, grid_space, device,maxB=maxB,minB=minB)
+          rmse_val,loss_val,Rsquare = check_rmse_CNN(valid_loader,model,grid_space, device, DF,maxB=maxB,minB=minB)
+          rmse,loss_train,R_TEMP = check_rmse_CNN(train_loader,model, grid_space, device, DF,maxB=maxB,minB=minB)
           rmse_val_history[tt//print_every] = rmse_val
           rmse_history[tt // print_every] = rmse 
           iter_history[tt // print_every] = tt 
@@ -310,8 +310,8 @@ def train_part_GM(model,optimizer,train_loader,valid_loader, epochs = 1, learnin
             
         elif not verbose and (t == len(train_loader)-1):
           print(f'Epoch {epoch:d}, Iteration {tt:d}, loss = {loss.item():.4f}')
-          rmse_val,mse_val,Rsquare= check_rmse_CNN(valid_loader,model, grid_space, DF,device,maxB=maxB,minB=minB)
-          rmse,mse_train,R_TEMP = check_rmse_CNN(train_loader,model, grid_space, DF,device,maxB=maxB,minB=minB)
+          rmse_val,mse_val,Rsquare= check_rmse_CNN(valid_loader,model, grid_space, device,DF,maxB=maxB,minB=minB)
+          rmse,mse_train,R_TEMP = check_rmse_CNN(train_loader,model, grid_space, device,DF,maxB=maxB,minB=minB)
           rmse_val_history[epoch] = rmse_val
           rmse_history[epoch] = rmse 
           iter_history[epoch] = tt 
@@ -423,7 +423,7 @@ def get_mean_of_dataloader(dataloader,model,device):
     return b/num_samples
 
 
-def check_rmse_CNN(dataloader,model, grid_space, device, verbose=False):
+def check_rmse_CNN(dataloader,model, grid_space, device, DF, verbose=False, maxB=[],minB=[]):
     mse_temp = 0
     R_temp=0
     Rsquare=0
